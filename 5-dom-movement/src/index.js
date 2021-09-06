63dc8c5d5c3d0860ee9d350ef705a04f4a9f8967
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2021-09-06 08:40:16 -0500

    Completed exercise 5

--- 5-dom-movement/src/index.js
+++ 5-dom-movement/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+	const [pixels, setPixels] = useState(300);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	return (
+		<div className="App">
+			<h1>Move the Box!</h1>
+
+			{/* handle the click event on this button */}
+			<button
+				onClick={() => {
+					setPixels(pixels - 50);
+				}}
+			>
+				👆 Move Up 👆
+			</button>
+
+			{/* move this box using inline styles */}
+			<div className="box" style={{ transform: `translateY(${pixels}px)` }} />
+		</div>
+	);
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
